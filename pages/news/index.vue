7c6d333b67e97c7af90271d5338d371a24e8a699
ot-nemoto <nemoto@opentone.co.jp>
2023-10-05 06:16:23 +0000

    コンテンツ一覧/詳細ページを作成する

---
+++ pages/news/index.vue
@@ -0,0 +1,20 @@
+<template>
+    <div>
+      <p>News list</p>
+      <div v-for="n in response.list" :key="n.slug">
+        <nuxt-link :to="`/news/${n.topics_id}`">
+          {{ n.ymd }} {{ n.subject }}
+        </nuxt-link>
+      </div>
+    </div>
+  </template>
+
+  <script>
+  export default {
+    async asyncData({ $axios }) {
+      return {
+        response: await $axios.$get('/rcms-api/5/news'),
+      };
+    },
+  };
+  </script>
